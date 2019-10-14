c3cd5e8de9563571dbe67b9b9c125735baf9bc4a
Flynn <crf204@gmail.com>
2019-10-13 21:34:40 -0400

    webster test

--- tests/test_apportionment.py
+++ tests/test_apportionment.py
@@ -1,4 +1,5 @@
 """Apportionment method tests."""
+from voting.apportionment import webster
 
 
 def test_apportionment_sum(apportionment_method, votes, seats):
@@ -7,3 +8,7 @@ def test_apportionment_sum(apportionment_method, votes, seats):
 
 def test_apportionment_values(apportionment_method, votes, seats_val):
     assert sum(apportionment_method(votes, seats_val)) == seats_val
+
+
+def test_webster():
+    assert all([a >= 0 for a in webster([2057, 2496, 2059, 181], 15)])
